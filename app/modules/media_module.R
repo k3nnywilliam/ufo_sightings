9099ce12a1baeb70c0f8f6564f74a9cfb189dd5a
Kenny William Nyallau <you@example.com.env>
2023-11-22 19:26:24 +0800

    Added media module, updated styles

--- app/modules/media_module.R
+++ app/modules/media_module.R
@@ -2,10 +2,29 @@ ui_module_for_media <- function(id) {
   ns <- NS(id)
   tabPanel(
     "Media",
+    br(),br(),
     fluidRow(
-      column(),
-      column(),
-      column(),
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/auITEKd4sjA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+             ),
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/Wh-tCAY0u7I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+             ),
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/2nSIKYlm2eE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+             ),
+    ),
+    br(), br(),
+    fluidRow(
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/YxDYWh6FAfw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+      ),
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/SNgoul4vyDM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+      ),
+      column(4,
+             HTML('<iframe width="450" height="315" src="https://www.youtube.com/embed/w-xJATBctIA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+      ),
     )
   )
 }
